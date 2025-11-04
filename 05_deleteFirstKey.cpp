5144089b8d41d7e61646a99ca71d5c4cd842e818
upma77 <upmakumarikaimur@gmail.com>
2025-11-04 16:55:30 +0530

    deleting head element

---
+++ 05_deleteFirstKey.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node* next;
+
+    Node(int data) {
+        val = data;
+        next = NULL;
+    }
+};
+
+void insertAtHead(Node* &head, int val) {
+    Node* new_node = new Node(val);
+    new_node->next = head;
+    head = new_node;
+}
+
+void deleteAtHead(Node* &head){
+    Node* temp = head;
+    head = head->next;
+    free(temp);
+}
+
+
+void display(Node* head) {
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->val << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main() {
+    Node* head = NULL;
+    insertAtHead(head, 2);
+    display(head);
+
+    insertAtHead(head, 1);
+    display(head);
+
+    insertAtHead(head, 3);
+    display(head);
+
+    deleteAtHead(head);
+    display(head);
+
+    return 0;
+}
