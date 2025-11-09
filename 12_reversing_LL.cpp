8a90bb5dc84c8bfaf3be0c40d21312be1610a48d
upma77 <upmakumarikaimur@gmail.com>
2025-11-09 10:58:05 +0530

    reversing linked list by recursive

---
+++ 12_reversing_LL.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node* next;
+
+    Node(int data) {
+        val = data;
+        next = NULL;
+    } 
+};
+
+class LinkedList {
+public:
+    Node* head;
+
+    LinkedList() {
+        head = NULL;
+    }
+
+    void insertAtTail(int value) {
+        Node* new_node = new Node(value);
+        if (head == NULL) {
+            head = new_node;
+            return;
+        }
+
+        Node* temp = head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = new_node;
+    }
+
+    void display() {
+        Node* temp = head;
+        while (temp != NULL) {
+            cout << temp->val << "->";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+};
+
+Node* reverseLLRecursion(Node* &head){
+    if(head == NULL || head->next == NULL){
+        return head;
+    }
+
+    Node* new_head = reverseLLRecursion(head->next);
+    head->next->next = head;
+    head->next = NULL;
+    return new_head;
+}
+
+int main() {
+    LinkedList ll;
+    ll.insertAtTail(1);
+    ll.insertAtTail(2);
+    ll.insertAtTail(3);
+    ll.insertAtTail(4);
+    ll.insertAtTail(5);
+    ll.insertAtTail(6);
+    
+    cout << "Original List: ";
+    ll.display();
+
+    ll.head = reverseLLRecursion(ll.head);  //  Update the head
+    cout << "Reversed List: ";
+    ll.display();
+    
+    return 0;
+}
